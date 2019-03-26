d69397c61f22790560a90a462924b4d8aac4dc6b
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-03-26 08:55:46 +0700

    Add files via upload

---
+++ LatihanClassDanObject-20190326T014012Z-001/LatihanClassDanObject/Mahasiswa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+    
+    namespace LatihanClassDanObject
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("Nim: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("\nTelah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang melakukan Proses Krs\n", Nama );
+        }
+    }
+}
---
+++ LatihanClassDanObject-20190326T014012Z-001/LatihanClassDanObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "18112276";
+            saya.Nama = "Indriana";
+            saya.Ipk = 3.92f;
+
+            kamu.Nim = "18112297";
+            kamu.Nama = "Anggi";
+            kamu.Ipk = 3.95f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
